b4a7d7c03b3968fb093a4ccf584c0b1f5677fb4e
Peter Bilberg <peter.langbo.bilberg.beumer@gmail.com>
2024-12-25 12:34:19 +0100

    clean project with batch

---
+++ src/main/java/com/batch/exsample/batch_demo2/BatchDemo2Application.java
@@ -0,0 +1,13 @@
+package com.batch.exsample.batch_demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchDemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch/exsample/batch_demo2/BatchDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.exsample.batch_demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
